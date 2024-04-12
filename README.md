4ff8023b65583254223cfd9abc221ffecc4b6cc7
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-04-11 20:17:42 -0400

    Use hacks to allow mods to be placed directly in the mods folder

--- README.md
+++ README.md
@@ -7,13 +7,4 @@ A simple utility to make running other people's mods in dev easier on Minecraft 
 Download the latest version from https://nightly.link/TauMC/Taufuscator/workflows/build-snapshot/main/Taufuscator.zip,
 extract the zip, and place the included JAR in the `mods` folder of your development workspace (usually `run/mods`).
 
-Create a `mods.deobf` folder beside that one and place valid, obfuscated mod JAR files in that folder. The folder
-structure should look like:
-```
-mods
-└── taufuscator.jar
-mods.deobf
-└── create-1.20.1.jar
-```
-
-Lastly, launch the game as normal and mods in `mods.deobf` should load and work normally.
+You can now place obfuscated mod JAR files in the mods folder as usual, and they will be remapped at launch time.
--- src/main/java/org/taumc/fmldeobfuscator/RuntimeDeobfLocator.java
+++ src/main/java/org/taumc/fmldeobfuscator/RuntimeDeobfLocator.java
@@ -1,19 +1,25 @@
 package org.taumc.fmldeobfuscator;
 
+import com.google.common.hash.Hashing;
 import cpw.mods.modlauncher.api.LamdbaExceptionUtils;
+import cpw.mods.modlauncher.api.NamedPath;
 import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.fml.loading.ModDirTransformerDiscoverer;
 import net.minecraftforge.fml.loading.moddiscovery.AbstractJarFileModLocator;
+import net.minecraftforge.fml.loading.moddiscovery.ModsFolderLocator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
@@ -23,9 +29,14 @@ import java.util.stream.Stream;
 
 public class RuntimeDeobfLocator extends AbstractJarFileModLocator {
     private static final int DISK_VERSION = 1;
-    private final Path modsToDeobfFolder = FMLPaths.GAMEDIR.get().resolve("mods.deobf");
+    private final Path modsToDeobfFolder = FMLPaths.GAMEDIR.get().resolve("mods");
     private final Path cachedDeobfFolder = FMLPaths.GAMEDIR.get().resolve(".taufuscator_v" + DISK_VERSION);
     public static final Logger LOGGER = LogManager.getLogger("Taufuscator");
+
+    private static String computeHash(Path path) throws IOException {
+        String hash = com.google.common.io.Files.asByteSource(path.toFile()).hash(Hashing.sha256()).toString();
+        return hash.substring(0, Math.min(hash.length(), 32));
+    }
 
     @Override
     public Stream<Path> scanCandidates() {
@@ -48,20 +59,26 @@ public class RuntimeDeobfLocator extends AbstractJarFileModLocator {
         });
         List<Path> newMods = new ArrayList<>();
         for(Path path : pathList) {
-            Path relative = modsToDeobfFolder.relativize(path);
-            Path cached = cachedDeobfFolder.resolve(relative.getFileName().toString().toLowerCase(Locale.ROOT).replaceFirst("\\.jar$", "-deobf.jar"));
-            if(!Files.exists(cached)) {
-                LOGGER.info("Remapping {}...", path.getFileName().toString());
-                try(InputStream in = Files.newInputStream(path)) {
-                    try(OutputStream out = Files.newOutputStream(cached, StandardOpenOption.CREATE)) {
-                        ModRemapper.remapMod(in, out);
+            Path cached = null;
+            try {
+                String hash = computeHash(path);
+                cached = cachedDeobfFolder.resolve(hash + ".jar");
+                if(!Files.exists(cached)) {
+                    LOGGER.info("Remapping {}...", path.getFileName().toString());
+                    try(InputStream in = Files.newInputStream(path)) {
+                        try(OutputStream out = Files.newOutputStream(cached, StandardOpenOption.CREATE)) {
+                            ModRemapper.remapMod(in, out);
+                        }
                     }
-                } catch(IOException | RuntimeException e) {
-                    LOGGER.error("Mod {} will not be remapped", path, e);
-                    try { Files.deleteIfExists(cached); } catch(IOException ignored) {}
-                    continue;
                 }
+            } catch(IOException | RuntimeException e) {
+                LOGGER.error("Mod {} will not be remapped", path, e);
+                if(cached != null) {
+                    try { Files.deleteIfExists(cached); } catch(IOException ignored) {}
+                }
+                continue;
             }
+
             newMods.add(cached);
         }
         LOGGER.info("Found " + newMods.size() + " deobfuscated mods");
@@ -75,6 +92,20 @@ public class RuntimeDeobfLocator extends AbstractJarFileModLocator {
 
     @Override
     public void initArguments(Map<String, ?> arguments) {
-
+        LOGGER.info("Preventing discovery of any mods in the mods folder");
+        List<NamedPath> modList;
+        try(Stream<Path> fileList = Files.list(modsToDeobfFolder)) {
+            modList = fileList.map(path -> new NamedPath(path.getFileName().toString(), path)).collect(Collectors.toList());
+        } catch(IOException e) {
+            LOGGER.warn("Failed to read files from mod directory", e);
+            return;
+        }
+        try {
+            Field field = ModDirTransformerDiscoverer.class.getDeclaredField("found");
+            field.setAccessible(true);
+            ((List<NamedPath>)field.get(null)).addAll(modList);
+        } catch(ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
